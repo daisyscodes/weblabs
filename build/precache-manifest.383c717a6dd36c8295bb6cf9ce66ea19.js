b4a12fadd76a020ab5cb35e326007360c80f3b47
daisyscodes <katyapozharitskaya@gmail.com>
2022-06-19 19:35:35 +0300

    add front

---
+++ build/precache-manifest.383c717a6dd36c8295bb6cf9ce66ea19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9095b9886e113d841aeb98e29682bd69",
+    "url": "/bilyvio/bilyvio.github.io/index.html"
+  },
+  {
+    "revision": "00e79165a276567e081b",
+    "url": "/bilyvio/bilyvio.github.io/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "14e53d6cc2525985e391",
+    "url": "/bilyvio/bilyvio.github.io/static/css/main.e3cd3499.chunk.css"
+  },
+  {
+    "revision": "00e79165a276567e081b",
+    "url": "/bilyvio/bilyvio.github.io/static/js/2.37e6fc9d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bilyvio/bilyvio.github.io/static/js/2.37e6fc9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14e53d6cc2525985e391",
+    "url": "/bilyvio/bilyvio.github.io/static/js/main.624c7d25.chunk.js"
+  },
+  {
+    "revision": "d7127455d81d7db28bdb",
+    "url": "/bilyvio/bilyvio.github.io/static/js/runtime-main.ec552bf8.js"
+  }
+]);
